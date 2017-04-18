7c659567e8f49513559223a08d7f436324a2d268
TECNOSYLVA\fgarcia <fernando.garcia@xeridia.com>
2017-04-18 16:47:53 +0200

    Fixed bug in log model.

--- models/log.js
+++ models/log.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, DataTypes) => {
   var Log = sequelize.define('Log', {
     // general info
     level: { type: DataTypes.STRING(16), allowNull: false, defaultValue: '' },
-    message: { type: DataTypes.STRING(2048), allowNull: false, defaultValue: '' },
+    message: { type: DataTypes.STRING(4096), allowNull: false, defaultValue: '' },
     meta: { type: DataTypes.STRING(2048), allowNull: false, defaultValue: '' },
     // special timestamp field used by winston mysql logger
     timestamp: { type: DataTypes.DATE, allowNull: false, defaultValue: new Date() }
